52d0779754aad44aab7ffbb30226b9697b817db5
Yuvi2k22 <70070499+Yuvi2k22@users.noreply.github.com>
2024-08-17 23:39:02 +0530

    added required files (#1)

---
+++ my-portfolio/src/components/canvas/index.ts
@@ -0,0 +1,6 @@
+import StarsCanvas from './Stars'
+import EarthCanvas from './Earth'
+import ComputersCanvas from './Computers'
+import BallCanvas from './Ball'
+
+export { StarsCanvas, EarthCanvas, ComputersCanvas, BallCanvas}
