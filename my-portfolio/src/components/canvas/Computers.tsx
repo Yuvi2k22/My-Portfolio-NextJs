726570ad27a9581ceeebe0afdab1efae5927c6c7
Yuvi2k22 <yuvi2k22@gmail.com>
2024-08-29 23:33:37 +0530

    added hero of the page

--- my-portfolio/src/components/canvas/Computers.tsx
+++ my-portfolio/src/components/canvas/Computers.tsx
@@ -1,7 +1,80 @@
-export default function Computers() {
-    return (
-     <div>
-        Computers
-     </div>
-    );
-  }
+"use client";
+
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import * as THREE from 'three';
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
+import CanvasLoader from "../Loader";
+
+const Computers = ({ isMobile }: {isMobile: boolean}) => {
+  const computer = useGLTF("./desktop_pc/scene.gltf");
+
+  return (
+    <mesh>
+      <hemisphereLight intensity={1.5} groundColor='black' />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={4}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <pointLight intensity={5} />
+      <primitive
+        object={computer.scene}
+        scale={isMobile ? 0.5 : 0.65}
+        position={isMobile ? [-5.5, -2.75, -2.2] : [0, -3, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
+    </mesh>
+  );
+};
+
+const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event: { matches: boolean | ((prevState: boolean) => boolean); }) => {
+      setIsMobile(event.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
+  return (
+    <Canvas
+      frameloop='demand'
+      shadows
+      dpr={[1, 2]}
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+          mouseButtons={{
+            RIGHT: undefined, // Disable right-click drag
+            LEFT: THREE.MOUSE.ROTATE,
+            MIDDLE: undefined,
+          }}
+        />
+        <Computers isMobile={isMobile} />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputersCanvas;
