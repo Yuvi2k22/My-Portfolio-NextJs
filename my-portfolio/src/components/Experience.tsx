97f5749b6a599664624dd16c1f303db3fce9a61e
Yuvi2k22 <yuvi2k22@gmail.com>
2024-09-01 12:35:54 +0530

    add experience section

--- my-portfolio/src/components/Experience.tsx
+++ my-portfolio/src/components/Experience.tsx
@@ -1,7 +1,91 @@
-export default function Experience() {
-    return (
-     <div>
-        Experience
-     </div>
-    );
-  }
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import { motion } from 'framer-motion';
+import 'react-vertical-timeline-component/style.min.css';
+import { styles } from '../styles';
+import { experiences } from '../constants';
+import { SectionWrapper } from '../hoc';
+import { textVariant } from '../utils/motion';
+import Image from 'next/image';
+import { useInView } from 'react-intersection-observer';
+
+const ExperienceCard = ({ experience }: any) => {
+
+   const [hasBeenVisible, setHasBeenVisible] = useState(false);
+
+   const { ref, inView } = useInView({
+      threshold: 0.5,
+    });
+
+    useEffect(() => {
+      if (inView) {
+        setHasBeenVisible(true);
+      }
+    }, [inView])
+
+  return (
+    <VerticalTimelineElement
+      contentStyle={{ background: '#1d1836', color: '#fff' }}
+      contentArrowStyle={{ borderRight: '7px solid  #232631' }}
+      date={experience.date}
+      iconStyle={{ background: experience.iconBg }}
+      visible={hasBeenVisible}
+      icon={
+        <div ref={ref} className='flex justify-center items-center w-full h-full'>
+          <Image
+            src={experience.icon}
+            alt={experience.company_name}
+            className='w-[60%] h-[60%] object-contain'
+          />
+        </div>
+      }
+    >
+      <div ref={ref}>
+      <div>
+        <h3 className='text-white text-[24px] font-bold'>{experience.title}</h3>
+        <p className='text-secondary text-[16px] font-semibold' style={{ margin: 0 }}>
+          {experience.company_name}
+        </p>
+      </div>
+      <ul className='mt-5 list-disc ml-5 space-y-2'>
+        {experience.points.map((point: string | number | bigint | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | Promise<React.AwaitedReactNode> | null | undefined, index: any) => (
+          <li key={`experience-point-${index}`} className='text-white-100 text-[14px] pl-1 tracking-wider'>
+            {point}
+          </li>
+        ))}
+      </ul>
+      </div>
+    </VerticalTimelineElement>
+  );
+};
+
+const Experience = () => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={`${styles.sectionSubText} text-center`}>What I have done so far</p>
+        <h2 className={`${styles.sectionHeadText} text-center`}>Work Experience.</h2>
+      </motion.div>
+
+      <div className='mt-20 flex flex-col'>
+        {isClient && (
+          <VerticalTimeline>
+            {experiences.map((experience, index) => (
+              <ExperienceCard key={`experience-${index}`} experience={experience} />
+            ))}
+          </VerticalTimeline>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(Experience, 'work');
